5195e8fbb2f072ca0127c6affff755aa7ef91dd4
Louise Mair <nlm159@newcastle.ac.uk>
2018-11-12 14:11:45 +0000

    getting started

--- interlinkages_script.R
+++ interlinkages_script.R
@@ -3,3 +3,29 @@
 # ------------------------------ Aichi Interlinkages ----------------------------####
 
 # a script which we can hopefully share
+
+library(igraph)
+library(ggplot2)
+library(ggraph)
+
+## Links
+linkdf <- read.csv("Marques_interactions.csv")
+linkmat <- as.matrix(linkdf[,-1])
+rownames(linkmat) <- names(linkdf)[-1]
+colnames(linkmat) <- names(linkdf)[-1]
+linkmat
+
+## Make into network graph
+g <- graph_from_adjacency_matrix(linkmat, mode="directed", weighted=TRUE, diag=FALSE)
+
+## Quick plot (very messy! - just copied script from Mark)
+ggraph(g, layout="linear", circular=TRUE) +
+  geom_edge_arc(aes(width=weight, alpha=weight)) +
+  geom_node_point(aes(colour=name), size=15) +
+  geom_node_label(aes(label=name), size=3, repel=TRUE) +
+  guides(colour=FALSE) +
+  coord_fixed() +
+  theme_void()
+
+
+
